3fc0bcee7816ae690dc5ca3520f77f6990be3333
Alexandar Vysotskiy <alexandar.vysotskiy@gmail.com>
2021-04-30 13:52:17 +0300

    3.1 Task

---
+++ src/test/java/third/ThirdTest.groovy
@@ -0,0 +1,36 @@
+package third
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class ThirdTest extends Specification {
+
+    @Shared third = new Third()
+
+    private static final String path = 'src\\main\\resources\\third'
+    private static final String newFileOne = '3216.dat'
+    private static final String newFileTwo = '5432115.dat'
+
+    def setup() {
+        new File(path).mkdir()
+        new File("""${path}/123.txt""").createNewFile()
+        new File("""${path}/12345.txt""").createNewFile()
+    }
+
+    def cleanup() {
+       new File(path).deleteDir()
+    }
+
+    def "find"() {
+        File[] result = third.find(path)
+
+        expect:
+        !result.toList().isEmpty()
+
+        and:
+        result[0].name == newFileOne
+
+        and:
+        result[1].name == newFileTwo
+    }
+}
