8a28d43ad7abdddfb3396167f2efeaef6e5eeffa
Alexandar Vysotskiy <alexandar.vysotskiy@gmail.com>
2021-05-04 15:41:49 +0300

    Fourth test fixing

--- src/test/java/fourth/FourthDotThree.groovy
+++ src/test/java/fourth/FourthDotThree.groovy
@@ -1,6 +1,7 @@
 package fourth
 
 import fourth.Person.Address
+import spock.lang.Shared
 import spock.lang.Specification
 
 /**
@@ -16,45 +17,45 @@ import spock.lang.Specification
  */
 class FourthDotThree extends Specification {
 
+    @Shared
     Person first
+    @Shared
     Person second
+    @Shared
     Person third
+    @Shared
     Person fourth
+    @Shared
     Person fifth
 
-    def personList
+    @Shared
+    List<Person> personList
 
-    def setup() {
-        first = new Person(firstName: 'John', surName: 'Wick',
-                address: new Person.Address(city: 'Mensk', street: 'Garden', index: 123), 'age': 33)
+    def setupSpec() {
+        personList = [
+                first = new Person(firstName: 'John', surName: 'Wick',
+                        address: new Address(city: 'Mensk', street: 'Garden', index: 123), 'age': 33),
 
-        second = new Person(firstName: 'Vasya', surName: 'Pupkin',
-                address: new Person.Address(city: 'Brest', street: 'Gogolya', index: 99), 'age': 55)
+                second = new Person(firstName: 'Vasya', surName: 'Pupkin',
+                        address: new Address(city: 'Brest', street: 'Gogolya', index: 99), 'age': 55),
 
-        third = new Person(firstName: 'Eva', surName: 'Adam',
-                address: new Person.Address(city: 'Grodno', street: 'Pushkina', index: 22), 'age': 22)
+                third = new Person(firstName: 'Eva', surName: 'Adam',
+                        address: new Address(city: 'Grodno', street: 'Pushkina', index: 22), 'age': 22),
 
-        fourth = new Person(firstName: 'Nina', surName: 'Vasyna',
-                address: new Person.Address(city: 'Moscow', street: 'First', index: 111), 'age': 18)
+                fourth = new Person(firstName: 'Nina', surName: 'Vasyna',
+                        address: new Address(city: 'Moscow', street: 'First', index: 111), 'age': 18),
 
-        fifth = new Person(firstName: 'Olya', surName: 'Vasyna',
-                address: new Person.Address(city: 'Moscow', street: 'First', index: 111), 'age': 19)
-
-        personList = new ArrayList()
-        personList.add(first)
-        personList.add(second)
-        personList.add(third)
-        personList.add(fourth)
-        personList.add(fifth)
+                fifth = new Person(firstName: 'Olya', surName: 'Vasyna',
+                        address: new Address(city: 'Moscow', street: 'First', index: 111), 'age': 19)
+        ]
     }
 
     def "FirstTest"() {
-        List<Address> uniqueListAddress = personList.collect { Person p -> p.address }.unique()
-
+        when:
         def newFromPlus = fifth + second
         def newFromMinus = third - fourth
 
-        expect:
+        then:
         newFromPlus.firstName == 'Olya + Vasya'
         newFromPlus.age == 74
 
@@ -63,16 +64,19 @@ class FourthDotThree extends Specification {
         newFromMinus.age == 4
 
         and:
-        first.toString() == ("First name is: ${first.firstName} and surname is: ${first.surName}")
-        second.toString() == ("First name is: ${second.firstName} and surname is: ${second.surName}")
-        third.toString() == ("First name is: ${third.firstName} and surname is: ${third.surName}")
+        first.toString() == ("First name is: John and surname is: Wick")
+        second.toString() == ("First name is: Vasya and surname is: Pupkin")
+        third.toString() == ("First name is: Eva and surname is: Adam")
+    }
 
-        and:
-        personList.findAll { it -> it.age < 30 }[0].equals(third)
-        personList.findAll { it -> it.age < 30 }[1].equals(fourth)
-        personList.findAll { it -> it.age < 30 }[2].equals(fifth)
+    def "UniqueAddressTest"() {
+        given:
+        List<Address> uniqueListAddress = personList.collect { Person p -> p.address }
 
-        and:
+        when:
+        uniqueListAddress.unique()
+
+        then:
         uniqueListAddress.size() == 4
         uniqueListAddress[0] == first.address
         uniqueListAddress[1] == second.address
@@ -80,8 +84,19 @@ class FourthDotThree extends Specification {
         uniqueListAddress[3] == fifth.address
     }
 
+    def "FindAgeTest"() {
+        when:
+        List<Person> lessThirtyYearsPersons = personList.findAll { it -> it.age < 30 }
+
+        then:
+        lessThirtyYearsPersons[0].equals(third)
+        lessThirtyYearsPersons[1].equals(fourth)
+        lessThirtyYearsPersons[2].equals(fifth)
+    }
+
     def "SecondTest"() {
-        fifth.metaClass.toString = {-> "${surName} ${firstName} (${age})"}
+        given:
+        fifth.metaClass.toString = { -> "${surName} ${firstName} (${age})" }
 
         expect:
         fifth.toString() == 'Vasyna Olya (19)'
